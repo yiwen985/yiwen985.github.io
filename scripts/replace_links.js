468c6ecc4b1c3af9dd510abf37e3994bb19e2cf8
cliter <yiwen985@gmail.com>
2025-01-16 04:16:28 +0800

    update

--- scripts/replace_links.js
+++ scripts/replace_links.js
@@ -8,6 +8,7 @@ const linkMap = JSON.parse(fs.readFileSync(mapFilePath, 'utf8'));
 hexo.extend.filter.register('before_post_render', function(data) {
     // 遍历 linkMap，替换内容中的链接
     for (const [localLink, cdnLink] of Object.entries(linkMap)) {
+        if(cdnLink === '') continue;
         const regex = new RegExp(localLink, 'g');
         data.content = data.content.replace(regex, cdnLink);
         // console.log(localLink, cdnLink);
